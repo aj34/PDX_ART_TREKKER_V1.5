3900e44492d11f0e3836adb6463d4f7130910a05
April Johnson <ajohnson@extensis.com>
2015-08-02 23:25:47 -0700

    First commit for V1.5 (updated cordova)
    Cordova version 5.1.1

---
+++ www/js/index.js
@@ -0,0 +1,524 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+Parse.initialize("key", "key");
+ArtObject = Parse.Object.extend("art");
+
+var artItems = [];
+var googlePlaceResults = [];
+
+var queryRequestCount = 0;
+
+var placeRecordId = 0;
+var viewAssembler = new ViewAssembler();
+
+var infoBubble;
+var map;
+var pdxLatLng;
+var currentLat;
+var currentLng;
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        $.getScript('https://maps.googleapis.com/maps/api/js?key=mykey&sensor=true&libraries=places&callback=onMapsApiLoaded');
+
+        document.addEventListener("backbutton", onBackKey, false);
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+
+(function (global) {
+    "use strict";
+
+    global.onMapsApiLoaded = function () {
+        // Maps API loaded and ready to be used.
+        pdxLatLng = new google.maps.LatLng(45.522886, -122.677631);
+
+        loadTemplates( setupDefaultView );
+    };
+
+})(window);
+
+function getArtItems (skipNum) {
+    var query = new Parse.Query(ArtObject);
+    query.limit(100);
+    query.skip(skipNum);
+
+    query.find({
+        success:function(results) { onParseSuccess(results); },
+        error: function(error) { onParseError(error); }
+    });
+}
+
+function getMyLocation() {
+    if (navigator.geolocation) {
+        var options = { maximumAge: 5000, timeout: 5000, enableHighAccuracy: true };
+        navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError, options);
+    } else {
+        alert('Geolocation is not supported. We will use Downtown Portland, OR as your current location.');
+        currentLat = 45.522886;
+        currentLng = -122.677631;
+    }
+}
+
+function onGeoSuccess(position) {
+    currentLat = parseFloat( position.coords.latitude );
+    currentLng = parseFloat( position.coords.longitude );
+
+    var marker = new google.maps.Marker({
+        position:new google.maps.LatLng(currentLat, currentLng),
+        map:map,
+        title: "You are here!"
+    });
+}
+
+
+function onGeoError(error) {
+//        alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+    // error.code can be:
+    //   0: unknown error
+    //   1: permission denied
+    //   2: position unavailable (error response from locaton provider)
+    //   3: timed out
+}
+
+
+function setupDefaultView() {
+
+    var mapView = {view: viewAssembler.mapArtView(),
+        title: "PDX Art Trekker",
+        scroll:false
+    };
+
+    //Setup the ViewNavigator
+    window.viewNavigator = new ViewNavigator( 'body' );
+    window.viewNavigator.pushView( mapView );
+}
+
+
+function onParseSuccess(results) {
+    for (var i = 0; i < results.length; i++) {
+        artItems.push(results[i]);
+    }
+
+    if (results.length > 0){
+        createArtMarkers(results);
+        preloadImages(results);
+    }
+
+    if (results.length == 100){
+        ++queryRequestCount;
+        getArtItems(100 * queryRequestCount);
+    } else {
+        removeLoadingMsg();
+        getGooglePlaces();
+    }
+}
+
+function getGooglePlaces() {
+    // get google places
+    var request = {
+        location: pdxLatLng,
+        radius: 8000,
+        types:['museum','art_gallery']
+    };
+
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+}
+
+function callback(results, status, pagination) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        createGooglePlaceMarkers(results);
+        if (pagination.hasNextPage) {
+            setTimeout( function () {
+                pagination.nextPage();
+            }, 100 );
+        }
+    } else {
+        alert("We encountered an error when retrieving the google places data.");
+    }
+    getMyLocation();
+}
+
+function onParseError(error) {
+    alert("We encountered an error when retrieving the data: " + error.code + " " + error.message);
+    removeLoadingMsg();
+}
+
+function removeLoadingMsg() {
+    // wait about 2 more seconds before removing this msg
+    setTimeout( function () {
+        if (document.getElementById("loadingData")){
+            document.getElementById("loadingData").style.display = 'none';
+        }
+    }, 2000 );
+}
+
+function preloadImages(items) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    for (var i = 0; i < items.length; i++) {
+        var art = items[i];
+        var img = new Image();
+        img.src = art.attributes.image_url;
+        preloadImages.list.push(img);
+    }
+}
+
+function onAboutViewClick( event ) {
+    var view = { title: "About",
+        view: viewAssembler.aboutView(),
+        backLabel: " "
+    };
+    window.viewNavigator.pushView( view );
+    event.stopPropagation();
+    return false;
+}
+
+function onGalleryViewClick( event ) {
+    var view = { title: "Gallery",
+        view: viewAssembler.galleryView(0, 50, artItems, false, true),
+        backLabel: " "
+    };
+    window.viewNavigator.pushView( view );
+
+    event.stopPropagation();
+    return false;
+}
+
+function onFavViewClick( event ) {
+    populateFavoritesGallery(null);
+    event.stopPropagation();
+    return false;
+}
+
+function populateFavoritesGallery(option) {
+    if (option == 'clear') {
+        var result=confirm("Are you sure you want to delete all your favorites?");
+        if (result==true) {
+            localStorage.clear();
+        } else {
+            return;
+        }
+    }
+
+    var favs = [];
+    for (key in localStorage){
+        if (localStorage.getItem(key) == "artRecordId") {
+            var artItem = getArtItemFromRecordId(key);
+            if (artItem != null) {
+                favs.push(artItem);
+            }
+        }
+    }
+
+    var view = { title: "Favorites",
+        view: viewAssembler.galleryView(0, favs.length, favs, true, false),
+        backLabel: " "
+    };
+
+    if (option =='refresh' || option == 'clear') {
+        view.view.children().remove();
+        view.view.append( viewAssembler.galleryView(0, favs.length, favs, true, false));
+        window.viewNavigator.replaceView( view );
+    } else {
+        if (favs.length > 0) {
+            window.viewNavigator.pushView( view );
+        } else {
+            alert("You have not selected any favorites.");
+        }
+    }
+}
+
+function getMoreImagesInGallery(startIndex, endIndex) {
+
+    var view = { title: "Gallery",
+        view: viewAssembler.galleryView(startIndex, endIndex, artItems, false, true),
+        backLabel: " "
+    };
+
+    view.view.children().remove();
+    view.view.append( viewAssembler.galleryView(startIndex, endIndex, artItems, false, true));
+    window.viewNavigator.replaceView( view );
+}
+
+function distance( lat1, lon1, lat2, lon2 ) {
+    var Rk = 6371; // Radius of the earth in km
+    var Rm = 3961; // Radius pf the earth in miles
+    var dLat = toRad(lat2-lat1);  // Javascript functions in radians
+    var dLon = toRad(lon2-lon1);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = Rk * c; // Distance in km
+    var m = Rm * c; // Distance in miles
+    return m;
+}
+
+function toRad(degree)
+{
+    rad = degree* Math.PI/ 180;
+    return rad;
+}
+
+function getDisciplineColor(discipline) {
+
+    var marker = "yellow"
+
+    if (discipline != null){
+        switch (discipline.toLowerCase()){
+            case "sculpture":
+                marker = "blue";
+                break;
+            case "painting":
+                marker = "green";
+                break;
+            case "ceramic":
+            case "fiber":
+                marker = "lightblue";
+                break;
+            case "architectural integration":
+                marker = "purple";
+                break;
+            case "mural":
+                marker = "pink";
+                break;
+            case "photography":
+                marker = "orange";
+                break;
+            default:
+                marker = "yellow"
+        }
+    }
+    return marker;
+}
+
+
+function showPlaceDetailsFromMapClick(recordId) {
+    infoBubble.close();
+
+    var foundPlace = false;
+    var place;
+    for (var i=0; i < googlePlaceResults.length; i++ ) {
+        place = googlePlaceResults[i];
+        if (place.recordId == recordId) {
+            foundPlace=true;
+            break;
+        }
+    }
+
+    if (foundPlace) {
+        var request = {
+            reference:place.reference
+        };
+
+        var service = new google.maps.places.PlacesService(map);
+        service.getDetails(request, placeDetailCallback);
+    }
+}
+
+function placeDetailCallback(placeDetailResult, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        var view = {title: "Details",
+            backLabel: " ",
+            view: viewAssembler.placeDetailView(placeDetailResult)
+        };
+        window.viewNavigator.pushView(view);
+    }
+}
+
+function getArtItemFromRecordId(recordId) {
+    for (var i=0; i < artItems.length; i++ ) {
+        var art = artItems[i];
+        if (art.attributes.record_id == recordId) {
+            return art;
+        }
+    }
+    return null;
+}
+
+function showArtDetailsFromMapClick(recordId) {
+    infoBubble.close();
+
+    var foundArt = false;
+    var art;
+    for (var i=0; i < artItems.length; i++ ) {
+        art = artItems[i];
+        if (art.attributes.record_id == recordId) {
+            foundArt=true;
+            break;
+        }
+    }
+
+    if (foundArt) {
+        var view = {title: "Details",
+            backLabel: " ",
+            view: viewAssembler.artDetailView(art)
+        };
+        window.viewNavigator.pushView(view);
+    }
+}
+
+function onArtListItemClick( event ) {
+    var target = $( event.target )
+    if (target.get(0).nodeName.toUpperCase() != "LI") {
+        target=target.parent();
+    }
+    var recordId = target.attr( "index" );
+    recordId = parseInt( recordId );
+    showArtDetailsFromMapClick( recordId );
+}
+
+
+function onArtListViewClick( event ) {
+
+    setTimeout( function(){
+        var view = { title: "Nearby Art",
+            view: viewAssembler.artListView(),
+            backLabel: (" "),
+            scroll:true
+        };
+
+        window.viewNavigator.pushView( view );
+    }, 500 );
+
+    event.stopPropagation();
+    return false;
+}
+
+
+function openExternalURL( url ) {
+    var result=confirm("You will leave the PDX Art Trekker App. Continue?");
+    if (result==true) {
+        window.open(url, '_blank', 'location=yes');
+    }
+}
+
+function addToFav(add, recordId ) {
+    if(typeof(Storage)!=="undefined"){
+        if (add) {
+            localStorage.setItem(recordId, "artRecordId");
+        } else {
+            localStorage.removeItem(recordId);
+        }
+    } else {
+//        console.log("browser does not support local storage");
+    }
+}
+
+
+function createArtMarkers(items) {
+    for (var i = 0; i < items.length; i++) {
+        var art = items[i];
+
+        var geo = new google.maps.LatLng(art.attributes.lat, art.attributes.lng);
+
+        var mapIcon = "assets/graphics/map_icons/" + getDisciplineColor(art.attributes.discipline) + ".png";
+
+        var marker = new google.maps.Marker({
+            map:map,
+            position:geo,
+            icon:mapIcon
+        });
+
+        // Creating a closure to retain the correct data
+        (function (marker, art) {
+
+            // Attaching a click event to the current marker
+            google.maps.event.addListener(marker, "click", function () {
+                var img = "<a href='javascript:showArtDetailsFromMapClick(" + art.attributes.record_id + ");'>" +
+                  "<div id='artMarker'><img class='img-polaroid' src=" + art.attributes.image_url + " width='144'/><p>" + art.attributes.title + "</p></div></a>";
+                infoBubble.setContent(img);
+                infoBubble.open(map, marker);
+            });
+        })(marker, art);
+    }
+}
+
+function createGooglePlaceMarkers(results) {
+    for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+
+        place.recordId = ++placeRecordId;
+
+        googlePlaceResults.push(place);
+
+        var icon = "assets/graphics/map_icons/brown_MarkerG.png";
+
+        for (var j = 0; j < place.types.length; j++) {
+            var type = place.types[j];
+            if (type == "museum"){
+                icon = "assets/graphics/map_icons/yellow_MarkerM.png";
+                break;
+            }
+        }
+
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location,
+            icon: icon
+        });
+
+        (function (marker, place) {
+            google.maps.event.addListener(marker, 'click', function() {
+                var img = "<a href='javascript:showPlaceDetailsFromMapClick(" + place.recordId + ");'>"+"<div id='placesMarker'><p>" + place.name + "</p></div></a>";
+                infoBubble.setContent(img);
+                infoBubble.open(map, marker);
+            });
+        })(marker, place);
+    }
+}
+
+
+function onBackKey( event ) {
+
+    if ( window.viewNavigator.history.length > 1 ){
+        event.preventDefault();
+        window.viewNavigator.popView();
+
+        return false;
+    }
+    navigator.app.exitApp();
+}
---
+++ www/js/viewAssembler.js
@@ -0,0 +1,389 @@
+
+var templates = {
+    aboutViewTemplate:"views/aboutViewTemplate.html",
+    mapArtViewTemplate:"views/mapArtViewTemplate.html",
+    artListViewTemplate:"views/artListViewTemplate.html",
+    artDetailViewTemplate:"views/artDetailViewTemplate.html",
+    placeDetailViewTemplate:"views/placeDetailViewTemplate.html",
+    galleryViewTemplate:"views/galleryViewTemplate.html",
+    loaded: 0,
+    requested: 0
+};
+
+var ___templatesLoadedCallback;
+
+var touchStartColor = "#d9cf67";  //highlight yellow
+var touchEndColor = "#aaa660";   //gold
+
+function loadTemplates(callback) {
+    ___templatesLoadedCallback = callback;
+    
+    //load Mousetache HTML templates
+    for (var key in templates) {
+        (function() {
+             var _key = key.toString();
+             if ( _key != "loaded" && _key != "requested" ){
+                 templates.requested ++;
+                 
+                 var templateLoaded = function( template ){
+                    onTemplateLoaded( template, _key );
+                 }
+                 
+                 $.get( templates[ _key ], templateLoaded, "html" );
+             }
+         })();
+    }
+}
+
+function onTemplateLoaded(template, key) {
+    
+    //alert( key + ": " + template);
+    templates[ key ] = template;
+    templates.loaded ++;
+    
+    if ( templates.loaded == templates.requested ) {
+        ___templatesLoadedCallback();
+    }
+}
+
+function ViewAssembler() {
+ //   this.touchSupported = 'ontouchstart' in window;
+ //   this.CLICK_EVENT = this.touchSupported ? 'touchend' : 'click';
+    this.CLICK_EVENT = 'click';
+    return this;
+}
+
+ViewAssembler.prototype.aboutView = function() {
+    var el = $( templates.aboutViewTemplate );
+    return el;
+}
+
+ViewAssembler.prototype.mapArtView = function() {
+    var el = $( templates.mapArtViewTemplate );
+
+    el.find("#galleryButton").on( this.CLICK_EVENT, onGalleryViewClick );
+    el.find("#artNearbyButton").on( this.CLICK_EVENT, onArtListViewClick );
+    el.find("#favButton").on( this.CLICK_EVENT, onFavViewClick);
+    el.find("#aboutButton").on( this.CLICK_EVENT, onAboutViewClick );
+
+    el.find("#galleryButton").bind('touchstart', function(){
+        $("#galleryButton").css("background-color", touchStartColor);
+    }).bind('touchend', function(){
+            $("#galleryButton").css("background-color", touchEndColor);
+        });
+
+    el.find("#artNearbyButton").bind('touchstart', function(){
+        $("#artNearbyButton").css("background-color", touchStartColor);
+    }).bind('touchend', function(){
+            $("#artNearbyButton").css("background-color", touchEndColor);
+        });
+
+    el.find("#favButton").bind('touchstart', function(){
+        $("#favButton").css("background-color", touchStartColor);
+    }).bind('touchend', function(){
+            $("#favButton").css("background-color", touchEndColor);
+        });
+
+    el.find("#aboutButton").bind('touchstart', function(){
+        $("#aboutButton").css("background-color", touchStartColor);
+    }).bind('touchend', function(){
+            $("#aboutButton").css("background-color", touchEndColor);
+        });
+
+    setTimeout( function(){
+
+        var mapOptions = {
+            center:pdxLatLng,
+            zoom:14,
+            mapTypeId:google.maps.MapTypeId.ROADMAP,
+            zoomControl: true,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.LARGE
+            },
+            mapTypeControl: false
+        };
+
+ 		map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        infoBubble = new InfoBubble({
+            map: map,
+            shadowStyle: 1,
+            padding: 0,
+            backgroundColor: 'rgb(57,57,57)',
+            borderRadius:10,
+            borderWidth: 1,
+            borderColor: 'rgb(57,57,57)',
+            disableAutoPan: true,
+            hideCloseButton: true,
+            backgroundClassName: 'infoBubble'
+//           arrowPosition: 30,
+//           arrowStyle: 2
+//           arrowSize: 10,
+        });
+
+        // Listen for user click on map to close any open info bubbles
+        google.maps.event.addListener(map, "click", function () {
+            infoBubble.close();
+        });
+    }, 300 );
+
+
+    setTimeout( function(){
+        getArtItems();
+    }, 3000 );
+
+    return el;
+}
+
+
+function getValidGalleryIndex (index) {
+    index = index + 50;
+
+    if (index > artItems.length) {
+        index = artItems.length;
+    }
+    return index;
+}
+
+ViewAssembler.prototype.galleryView = function(startIndex, endIndex, items, showRefreshButton, showMoreImagesButton) {
+    var template = templates.galleryViewTemplate;
+
+    var sIndex = getValidGalleryIndex(parseInt(startIndex));
+    var eIndex = getValidGalleryIndex(parseInt(endIndex));
+
+    if (sIndex == artItems.length && eIndex == artItems.length ){
+        sIndex = 0;
+        eIndex = 50;
+    }
+
+    var viewModel = {
+        startIndex: sIndex,
+        endIndex:eIndex
+    };
+
+    var el = $( Mustache.to_html(template, viewModel) );
+
+    if (!showRefreshButton){
+        el.find( ".galleryButtonBlock").css( "display","none" );
+    } else {
+        el.find("#refreshButton").bind('touchstart', function(){
+            $("#refreshButton").css("background-color", touchStartColor);
+        }).bind('touchend', function(){
+                $("#refreshButton").css("background-color", touchEndColor);
+            });
+
+        el.find("#clearButton").bind('touchstart', function(){
+            $("#clearButton").css("background-color", touchStartColor);
+        }).bind('touchend', function(){
+                $("#clearButton").css("background-color", touchEndColor);
+            });
+    }
+
+    if (!showMoreImagesButton){
+        el.find( ".moreImagesButtonBlock").css( "display","none" );
+    } else {
+        el.find("#moreImagesButton").bind('touchstart', function(){
+            $("#moreImagesButton").css("background-color", touchStartColor);
+        }).bind('touchend', function(){
+                $("#moreImagesButton").css("background-color", touchEndColor);
+            });
+    }
+
+    var markupString = '';
+    for (var i=parseInt(startIndex); i< parseInt(endIndex); i++) {
+        var art = items[i];
+        markupString += "<div class='image-frame'><a href='javascript:showArtDetailsFromMapClick(" + art.attributes.record_id +");'>" +
+                        "<img id='galleryThumbnail' src='" + art.attributes.image_url +"'/></a></div>"
+    }
+
+    el.find("#galleryWrapper").append(markupString);
+
+    return el;
+}
+
+ViewAssembler.prototype.favView = function() {
+    var el = $( templates.favViewTemplate );
+    return el;
+}
+
+ViewAssembler.prototype.artListView = function() {
+    var template = templates.artListViewTemplate;
+
+    var result = [];
+    for (var i=0; i< artItems.length; i++) {
+        var art = artItems[i];
+        var artListInfo = [];
+
+        var lat1 = parseFloat(art.attributes.lat);
+        var lon1 = parseFloat(art.attributes.lng);
+        var lat2 = parseFloat(currentLat);
+        var lon2 = parseFloat(currentLng);
+
+        if (isNaN(lat1) || isNaN(lon1) || isNaN(lat2) || isNaN(lon2)){
+            // don't add artListInfo.distance item
+        } else {
+            artListInfo.distance = Math.round(distance(lat1, lon1, lat2, lon2) * 1000)/1000;
+        }
+        artListInfo.title = art.attributes.title;
+        artListInfo.artist = art.attributes.artist;
+        artListInfo.image = art.attributes.image_url;
+//        artListInfo.disciplineColor = getDisciplineColor(art.attributes.discipline);
+        artListInfo.recordId = art.attributes.record_id;
+        result.push(artListInfo);
+    }
+
+    result.sort( function(a, b){
+        if ( a.distance < b.distance) { return -1; }
+        else if (a.distance> b.distance ) { return 1; }
+        else return 0;
+    });
+
+    // get the closest 50 items
+    var artNearby = [];
+    for (var i=0; i < 50; i++) {
+        var art = result[i];
+        artNearby.push(art);
+    }
+
+    var viewModel = {
+        artList: artNearby
+    };
+
+    var el = $( Mustache.to_html(template, viewModel) );
+    el.find( "li" ).on( this.CLICK_EVENT, onArtListItemClick );
+    return el;
+}
+
+
+ViewAssembler.prototype.artDetailView = function(art) {
+    var template = templates.artDetailViewTemplate;
+
+/*
+    art.attributes.disciplineColor = getDisciplineColor(art.attributes.discipline);
+*/
+
+    var el = $( Mustache.to_html(template, art.attributes));
+
+    if (art.attributes.street == art.attributes.location){
+        el.find( "#location").css( "display","none");
+    }
+
+    el.find("#addToFav").bind('touchstart', function(){
+        $("#addToFav").css("background-color", touchStartColor);
+    }).bind('touchend', function(){
+            $("#addToFav").css("background-color", touchEndColor);
+        });
+
+    el.find("#removeFromFav").bind('touchstart', function(){
+        $("#removeFromFav").css("background-color", touchStartColor);
+    }).bind('touchend', function(){
+            $("#removeFromFav").css("background-color", touchEndColor);
+        });
+
+
+    setTimeout( function(){
+        var geo = new google.maps.LatLng(art.attributes.lat, art.attributes.lng);
+
+        var mapOptions = {
+            center:geo,
+            zoom:15,
+            mapTypeId:google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: false,
+            draggable: false
+        };
+
+        var detailMapElement = el.get(0).children[1];
+        var artDetailMap = new google.maps.Map(detailMapElement, mapOptions);
+
+        var mapIcon = "assets/graphics/map_icons/" + getDisciplineColor(art.attributes.discipline) + ".png";
+
+        var artMarker = new google.maps.Marker({
+            map:artDetailMap,
+            position:geo,
+            icon:mapIcon
+        });
+
+        var YourLocation = new google.maps.Marker({
+            position:new google.maps.LatLng(currentLat, currentLng),
+            map:artDetailMap,
+            title: "You are here!"
+        });
+
+    }, 300 );
+
+    return el;
+}
+
+
+ViewAssembler.prototype.placeDetailView = function(place) {
+    var template = templates.placeDetailViewTemplate;
+
+    var icon = "assets/graphics/map_icons/brown_MarkerG.png";
+
+    for (var j = 0; j < place.types.length; j++) {
+        if (place.types[j] == "museum"){
+            icon = "assets/graphics/map_icons/yellow_MarkerM.png";
+            break;
+        }
+    }
+
+    place.placeMarkerIcon = icon;
+
+    var el = $( Mustache.to_html(template, place));
+
+    // hide reviews title if no reviews.
+    if (place.reviews && place.reviews.length > 0) {
+        // no op
+    } else  {
+        el.find( "#reviewsHeading").css( "display","none");
+    }
+
+    el.find("#websiteURL").bind('touchstart', function(){
+        $("#websiteURL").css("background-color", touchStartColor);
+    }).bind('touchend', function(){
+            $("#websiteURL").css("background-color", touchEndColor);
+        });
+
+    el.find("#plusPageURL").bind('touchstart', function(){
+        $("#plusPageURL").css("background-color", touchStartColor);
+    }).bind('touchend', function(){
+            $("#plusPageURL").css("background-color", touchEndColor);
+        });
+
+    setTimeout( function(){
+        var mapOptions = {
+            center:place.geometry.location,
+            zoom:15,
+            mapTypeId:google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: false,
+            draggable: false
+        };
+
+        var detailMapElement = el.get(0).children[1];
+        var placeDetailMap = new google.maps.Map(detailMapElement, mapOptions);
+
+        var placeMarker = new google.maps.Marker({
+            map:placeDetailMap,
+            position:place.geometry.location,
+            icon:icon
+        });
+
+        var YourLocation = new google.maps.Marker({
+            position:new google.maps.LatLng(currentLat, currentLng),
+            map:placeDetailMap,
+            title: "You are here!"
+        });
+
+    }, 300 );
+
+    return el;
+}
+
+
+
+
+
+
+
+
+
